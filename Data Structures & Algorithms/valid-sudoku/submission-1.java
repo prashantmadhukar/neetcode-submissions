18d1b1657df28f7a0e8092c983f353dcb71ade53
prashantmadhukar <42266758+prashantmadhukar@users.noreply.github.com>
2026-06-13 01:50:16 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> hs = new HashSet<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')
+                    continue;
+                String rowstr=board[i][j]+"@"+i +"row";
+                String colstr=board[i][j]+"@"+j +"col";
+                String boxstr=board[i][j]+"@"+i/3+j/3 +" box";
+                
+                if(hs.contains(rowstr)||hs.contains(colstr)||hs.contains(boxstr)){
+                    return false;
+                }else{
+                    hs.add(rowstr);
+                    hs.add(colstr);
+                    hs.add(boxstr);
+                }
+            }
+        }
+        return true;
+    }
+}
